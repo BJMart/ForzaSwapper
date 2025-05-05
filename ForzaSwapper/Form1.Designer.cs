628d3ba0dcf685cd6563fd261cc4a9a621c652a5
b mart <bjmartiensen27@gmail.com>
2025-05-06 00:06:23 +1000

    Add project files.

---
+++ ForzaSwapper/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ForzaSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            dgvSwaps = new DataGridView();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            textBox1 = new TextBox();
+            tabPage2 = new TabPage();
+            label4 = new Label();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)dgvSwaps).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(26, 6);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // dgvSwaps
+            // 
+            dgvSwaps.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvSwaps.Location = new Point(26, 95);
+            dgvSwaps.Name = "dgvSwaps";
+            dgvSwaps.Size = new Size(654, 305);
+            dgvSwaps.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(26, 35);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 0;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 60);
+            button1.TabIndex = 2;
+            button1.Text = "DB Location";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(153, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Engine";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(26, 64);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 25);
+            button2.TabIndex = 2;
+            button2.Text = "Swap";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(93, 3);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(706, 446);
+            tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(radioButton2);
+            tabPage1.Controls.Add(radioButton1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(698, 418);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Handling Mods";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(16, 31);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(16, 6);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(61, 19);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Level 1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(135, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(listBox1);
+            tabPage2.Controls.Add(dgvSwaps);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(comboBox1);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(comboBox2);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(698, 418);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Engine Swapper";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(153, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Car";
+            label4.Click += label4_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(211, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(469, 94);
+            listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvSwaps).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private DataGridView dgvSwaps;
+        private ComboBox comboBox2;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private RadioButton radioButton1;
+        private TextBox textBox1;
+        private RadioButton radioButton2;
+        private Label label4;
+        private ListBox listBox1;
+    }
+}
---
+++ ForzaSwapper/Form1.cs
@@ -0,0 +1,355 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace ForzaSwapper
+{
+    public partial class Form1 : Form
+    {
+        private string selectedEngineID = "";
+        private string VehicleID;
+        private string PathDB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Select a Filer";
+                ofd.Filter = "SLT Files (*.slt)|*.slt";
+                ofd.DefaultExt = "slt";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    PathDB = ofd.FileName;
+                    PopulateComboBox(comboBox1, "Data_Car", "MediaName");
+                    PopulateComboBox(comboBox2, "Data_Engine", "MediaName");
+                }
+            }
+        }
+
+        private void PopulateComboBox(ComboBox comboBox, string tableName, string columnName)
+        {
+            string connectionString = $@"Data Source={PathDB};Version=3;";
+            try
+            {
+                using var connection = new SQLiteConnection(connectionString);
+                connection.Open();
+
+                using var command = new SQLiteCommand(connection);
+                command.CommandText = $"SELECT * FROM List_UpgradeEngine";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                if (tableName == "Data_Car")
+                {
+                    command.CommandText = $"SELECT Id, {columnName} FROM {tableName} ORDER BY {columnName}";
+                    var items = new List<MediaItem>();
+                    using var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        items.Add(new MediaItem
+                        {
+                            Id = reader.GetInt32(0),
+                            MediaName = reader.GetString(1)
+                        });
+                    }
+                    comboBox.DataSource = items;
+                    comboBox.DisplayMember = "MediaName";
+                    comboBox.ValueMember = "Id";
+                }
+                else if (tableName == "Data_Engine")
+                {
+                    command.CommandText = $"SELECT EngineID, {columnName} FROM {tableName} ORDER BY {columnName}";
+                    var items = new List<MediaEngineItem>();
+                    using var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        items.Add(new MediaEngineItem
+                        {
+                            EngineID = reader["EngineID"].ToString(),
+                            MediaName = reader.GetString(1)
+                        });
+                    }
+                    comboBox.DataSource = items;
+                    comboBox.DisplayMember = "MediaName";
+                    comboBox.ValueMember = "EngineID";
+                }
+
+                dgvSwaps.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading {columnName} from {tableName}: {ex.Message}");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedValue is int selectedId)
+            {
+                LoadFilteredSwaps(selectedId);
+                VehicleID = selectedId.ToString();
+                UpdateCurrentEngineList();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem is MediaEngineItem engine)
+            {
+                selectedEngineID = engine.EngineID;
+               
+            }
+        }
+
+        private void LoadFilteredSwaps(int carId)
+        {
+            string query = "SELECT * FROM List_UpgradeEngine WHERE Ordinal = @carId";
+            try
+            {
+                using var connection = new SQLiteConnection($@"Data Source={PathDB};Version=3;");
+                using var command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@carId", carId);
+                var adapter = new SQLiteDataAdapter(command);
+                var filteredTable = new DataTable();
+                adapter.Fill(filteredTable);
+                dgvSwaps.DataSource = filteredTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error filtering swaps: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedEngineID) || string.IsNullOrEmpty(VehicleID))
+            {
+                MessageBox.Show($"Please select both a car and an engine.\nVehicleID: {VehicleID}\nEngineID: {selectedEngineID}");
+                return;
+            }
+
+            try
+            {
+                using var connection = new SQLiteConnection($@"Data Source={PathDB};Version=3;");
+                connection.Open();
+
+                // Get previous row
+                string getLastRowQuery = "SELECT Level, ManufacturerID FROM List_UpgradeEngine WHERE Ordinal = @vehicleID ORDER BY rowid DESC LIMIT 1";
+                var getLastCmd = new SQLiteCommand(getLastRowQuery, connection);
+                getLastCmd.Parameters.AddWithValue("@vehicleID", VehicleID);
+                object levelResult = null, manufacturerResult = null;
+
+                using (var reader = getLastCmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        levelResult = reader["Level"];
+                        manufacturerResult = reader["ManufacturerID"];
+                    }
+                }
+
+                int nextLevel = (levelResult != null && int.TryParse(levelResult.ToString(), out int lastLevel)) ? lastLevel + 1 : 1;
+                string formattedLevel = nextLevel.ToString("D3");
+                string generatedId = VehicleID + formattedLevel;
+                string manufacturerID = manufacturerResult?.ToString() ?? "";
+
+                // Get mass info
+                double selectedMass = GetEngineMass(connection, selectedEngineID);
+                string stockEngineID = GetStockEngineID(connection, VehicleID);
+                double stockMass = GetEngineMass(connection, stockEngineID);
+                double massDiff = selectedMass - stockMass;
+
+                // WeightDistDiff calculation
+                double weightDistDiff = CalculateWeightDistDiff(connection, VehicleID, massDiff);
+
+                // Price = 20% of base cost
+                double price = GetEngineSwapPrice(connection, selectedEngineID);
+
+                // Insert record
+                string insertQuery = @"
+INSERT INTO List_UpgradeEngine 
+(Id, Ordinal, EngineID, Level, IsStock, ManufacturerID, Price, MassDiff, WeightDistDiff, DragScale, WindInstabilityScale) 
+VALUES 
+(@id, @vehicleID, @engineId, @level, 0, @manufacturerID, @price, @massDiff, @weightDistDiff, 1, 1)";
+
+                using var insertCmd = new SQLiteCommand(insertQuery, connection);
+                insertCmd.Parameters.AddWithValue("@id", generatedId);
+                insertCmd.Parameters.AddWithValue("@vehicleID", VehicleID);
+                insertCmd.Parameters.AddWithValue("@engineId", selectedEngineID);
+                insertCmd.Parameters.AddWithValue("@level", formattedLevel);
+                insertCmd.Parameters.AddWithValue("@manufacturerID", manufacturerID);
+                insertCmd.Parameters.AddWithValue("@price", price);
+                insertCmd.Parameters.AddWithValue("@massDiff", massDiff);
+                insertCmd.Parameters.AddWithValue("@weightDistDiff", weightDistDiff);
+
+                if (insertCmd.ExecuteNonQuery() > 0)
+                    MessageBox.Show($"Record added:\nId: {generatedId}\nEngineID: {selectedEngineID}\nMassDiff: {massDiff}\nWeightDistDiff: {weightDistDiff}\nPrice: {price}");
+                else
+                    MessageBox.Show("Insert failed.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inserting record: " + ex.Message);
+            }
+        }
+
+        private double GetEngineMass(SQLiteConnection conn, string engineID)
+        {
+            if (string.IsNullOrEmpty(engineID)) return 0;
+            var cmd = new SQLiteCommand("SELECT [EngineMass-kg] FROM Data_Engine WHERE EngineID = @engineID", conn);
+            cmd.Parameters.AddWithValue("@engineID", engineID);
+            var result = cmd.ExecuteScalar();
+            return result != null && double.TryParse(result.ToString(), out double mass) ? mass : 0;
+        }
+
+        private string GetStockEngineID(SQLiteConnection conn, string vehicleID)
+        {
+            var cmd = new SQLiteCommand("SELECT EngineID FROM List_UpgradeEngine WHERE Ordinal = @vehicleID AND IsStock = 1 LIMIT 1", conn);
+            cmd.Parameters.AddWithValue("@vehicleID", vehicleID);
+            return cmd.ExecuteScalar()?.ToString() ?? "";
+        }
+
+        private double CalculateWeightDistDiff(SQLiteConnection conn, string vehicleID, double massDiff)
+        {
+            var cmd = new SQLiteCommand("SELECT MassDiff, WeightDistDiff FROM List_UpgradeEngine WHERE Ordinal = @vehicleID ORDER BY rowid DESC LIMIT 1", conn);
+            cmd.Parameters.AddWithValue("@vehicleID", vehicleID);
+            using var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                if (double.TryParse(reader["MassDiff"].ToString(), out double prevMassDiff) &&
+                    double.TryParse(reader["WeightDistDiff"].ToString(), out double prevDist) &&
+                    prevMassDiff != 0)
+                {
+                    return (prevDist / prevMassDiff) * massDiff;
+                }
+            }
+            return 0;
+        }
+
+        private double GetEngineSwapPrice(SQLiteConnection conn, string engineID)
+        {
+            string stockVehicleID = "";
+            var stockCmd = new SQLiteCommand("SELECT Ordinal FROM List_UpgradeEngine WHERE EngineID = @engineId AND IsStock = 1 LIMIT 1", conn);
+            stockCmd.Parameters.AddWithValue("@engineId", engineID);
+            var result = stockCmd.ExecuteScalar();
+            if (result != null)
+                stockVehicleID = result.ToString();
+
+            if (!string.IsNullOrEmpty(stockVehicleID))
+            {
+                var costCmd = new SQLiteCommand("SELECT BaseCost FROM Data_Car WHERE Id = @carId", conn);
+                costCmd.Parameters.AddWithValue("@carId", stockVehicleID);
+                var costResult = costCmd.ExecuteScalar();
+                if (costResult != null && double.TryParse(costResult.ToString(), out double baseCost))
+                    return baseCost * 0.2;
+            }
+
+            return 1;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void UpdateCurrentEngineList()
+        {
+            listBox1.Items.Clear();
+
+            // Query to get all EngineIDs under the selected Ordinal
+            string query = @"
+        SELECT EngineID 
+        FROM List_UpgradeEngine 
+        WHERE Ordinal = @selectedOrdinal";
+
+            string connectionString = $@"Data Source={PathDB};Version=3;";
+            try
+            {
+                using (var connection = new SQLiteConnection(connectionString))
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    // Use VehicleID (selected car) to filter by Ordinal
+                    command.Parameters.AddWithValue("@selectedOrdinal", VehicleID);
+                    connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string engineId = reader["EngineID"].ToString();
+
+                            // Query to get both MediaName (car name) and EngineName using the EngineID from Data_Engine
+                            string carNameQuery = "SELECT MediaName, EngineName FROM Data_Engine WHERE EngineID = @engineID";
+                            using (var carCommand = new SQLiteCommand(carNameQuery, connection))
+                            {
+                                carCommand.Parameters.AddWithValue("@engineID", engineId);
+
+                                // Execute the query to get both car name and engine name
+                                using (var carReader = carCommand.ExecuteReader())
+                                {
+                                    if (carReader.Read())
+                                    {
+                                        string carName = carReader["MediaName"].ToString();
+                                        string engineName = carReader["EngineName"].ToString();
+
+                                        // Format as carname(engineName) and add to listBox
+                                        listBox1.Items.Add($"{carName}({engineName})");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching car names with engine names: " + ex.Message);
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class MediaItem
+    {
+        public int Id { get; set; }
+        public string MediaName { get; set; }
+        public override string ToString() => MediaName;
+    }
+
+    public class MediaEngineItem
+    {
+        public string EngineID { get; set; }
+        public string MediaName { get; set; }
+        public override string ToString() => MediaName;
+    }
+}
